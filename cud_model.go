859c301cae11f16ebd0a04b1d9b96e30fe081745
zheng peng <pengzheng@huishoubao.com.cn>
2023-10-24 08:54:09 +0800

    add setModel

--- cud_model.go
+++ cud_model.go
@@ -17,7 +17,7 @@ func NewCUDModel(cudModelImpl CUDModelInterface) (cudModel *CUDModel) {
 
 // CUDUpdateHandleFn 增改删句柄函数
 type CUDUpdateHandleFn func(identifies ...string) (err error)
-type CUDAddHandleFn func() (identifies []string, err error)
+type CUDAddHandleFn func(uniqIds ...string) (identifies []string, err error)
 
 func (cudModel CUDModel) AddModel(addFn CUDAddHandleFn) (err error) {
 	ids, err := addFn()
@@ -53,6 +53,84 @@ func (cudModel CUDModel) UpdateModel(updateFn CUDUpdateHandleFn, ids ...string) 
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+// SetModel 筛选出新增发布创建事件，更新发布更新事件
+func (cudModel CUDModel) SetModel(addFn CUDAddHandleFn, updateFn CUDUpdateHandleFn, ids ...string) (err error) {
+	oldModels, err := cudModel.GetByIdentities(ids...)
+	if err != nil {
+		return err
+	}
+	updateIds := make([]string, 0)
+	for _, model := range oldModels {
+		updateIds = append(updateIds, model.GetIdentity())
+	}
+	addIds := make([]string, 0)
+	for _, id := range ids {
+		isUpdateId := false
+		for _, updateId := range updateIds {
+			isUpdateId = updateId == id
+			if isUpdateId {
+				break
+			}
+		}
+		if !isUpdateId {
+			addIds = append(addIds, id)
+		}
+	}
+	insertIds := make([]string, 0)
+	if len(addIds) > 0 {
+		insertIds, err = addFn(addIds...)
+		if err != nil {
+			return err
+		}
+	}
+	if len(updateIds) > 0 {
+		err = updateFn(updateIds...)
+		if err != nil {
+			return err
+		}
+	}
+
+	newmodels, err := cudModel.GetByIdentities(ids...)
+	if err != nil {
+		return err
+	}
+	insertModels := make(CUDEmiterInterfaces, 0)
+	updateModels := make(CUDEmiterInterfaces, 0)
+
+	for _, model := range newmodels {
+		isInsertModel := false
+		id := model.GetIdentity()
+		for _, insertId := range insertIds {
+			isInsertModel = insertId == id
+			if isInsertModel {
+				break
+			}
+		}
+
+		if isInsertModel {
+			insertModels = append(insertModels, model)
+		} else {
+			updateModels = append(updateModels, model)
+		}
+	}
+
+	if len(insertModels) > 0 {
+		err = EmitCreatedEvent(insertModels...)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(oldModels) > 0 && len(updateModels) > 0 {
+		err = EmitUpdatedEvent(oldModels, updateModels)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
